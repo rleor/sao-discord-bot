4404356ef2c48b338c574415bb79155127be5a78
LeoR <418266037@qq.com>
2022-06-29 16:35:47 +0800

    embed message

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ import {
   WALLET_VERIFY_COMMAND,
   HasGuildCommands,
 } from './commands.js';
+import {MessageEmbed} from 'discord.js';
+import moment from 'moment';
 
 // Create an express app
 const app = express();
@@ -34,28 +36,38 @@ app.post("/verify", express.json(), async function(req, res) {
   var r = await DiscordRequest(`guilds/${process.env.GUILD_ID}/roles`, {method: 'get'})
   const discordRoles = await r.json();
   console.log("all discord roles:", discordRoles);
+  var hasErr = false
   discordRoles.forEach(dr => {
     roles.forEach(async r => {
       if (r.toLowerCase() === dr.name.toLowerCase()) {
         var endpoint = `guilds/${process.env.GUILD_ID}/members/${discordUserId}/roles/${dr.id}`
         // TODO: discord request error handling.
         console.log("grant role: ", endpoint);
-        await DiscordRequest(endpoint, {method: 'put'});
+        try {
+          await DiscordRequest(endpoint, {method: 'put'});
+        } catch (e) {
+                console.log("assign role error:", e);
+                hasErr = true;
+        }
       }
     });
   });
 
-  return res.send(200);
+  if (hasErr) {
+          return res.send(500);
+  } else {
+          return res.send(200);
+  }
 });
 
 app.use(express.json({ verify: VerifyDiscordRequest(process.env.PUBLIC_KEY) }));
-
 
 // Store for in-progress games. In production, you'd want to use a DB
 
 /**
  * Interactions endpoint URL where Discord will send HTTP requests
  */
+
 app.post('/interactions', async function (req, res) {
   // Interaction type and data
   const { type, id, data } = req.body;
@@ -76,16 +88,33 @@ app.post('/interactions', async function (req, res) {
     const { name } = data;
 
     if (name === 'verify-wallet') {
+        console.log("verify-wallet");
       const userId = req.body.member.user.id;
+        //  content: `${process.env.VERIFY_BASE_URL}?user=${userId}`,
+      const exampleEmbed = new MessageEmbed()
+                .setColor('#0099ff')
+                .setTitle('Please read instructions carefully before connecting')
+                .setAuthor({ name: 'SAOBot', iconURL: 'https://www.gitbook.com/cdn-cgi/image/width=40,height=40,fit=contain,dpr=2,format=auto/https%3A%2F%2F3280704082-files.gitbook.io%2F~%2Ffiles%2Fv0%2Fb%2Fgitbook-x-prod.appspot.com%2Fo%2Fspaces%252FJ1lcmSyp0T4hJjEZ5QwT%252Ficon%252FpcUfhMSa30fnERzPTqsG%252Flogo-1.png%3Falt%3Dmedia%26token%3D564319b7-a85e-4ffd-aaab-bbeddc40c335'})
+                .setDescription('You should expect to sign the following message when prompted by a non-custodial wallet such as MetaMask:\n'+
+                        ' SAO asks you to sign this'+
+                        ' message for the purpose of verifying your account\n'+
+                        ' ownership. This is READ-ONLY access and will NOT trigger\n'+
+                        ' any blockchain transactions or incur any fees.\n'+
+                        ' \n'+
+                        '   - Community: SAO\n'+
+                        '   - User: '+ req.body.member.user.username +'#'+req.body.member.user.discriminator+'\n' +
+                        '   - Discord Interaction: ' + process.env.APP_ID + '\n' +
+                        '   - Timestamp: '+moment().format())
+                .setFooter({text: 'Make sure you sign the EXACT message(some wallets may use \\n for new lines)\nand NEVER share your seed phrase or private key.'});
       return res.send({
         type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
         data: {
-	  content: `Use this custom link to connect\nGuild: ${process.env.GUILD_ID} Members: ${userId}`,
+          content: `Use this custom link to connect\nGuild: ${process.env.GUILD_ID} Members: ${userId}`,
           components : [
                {
                    type: 1,
                    components : [
-                       {
+			                          {
                            type: 2,
                            label: "Connect Wallet",
                            style: 5,
@@ -94,6 +123,7 @@ app.post('/interactions', async function (req, res) {
                    ],
                },
            ],
+           embeds: [exampleEmbed],
         },
       });
     }
